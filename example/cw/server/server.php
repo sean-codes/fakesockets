e8a393a9de53734f304e40efb9182d100d09d099
sean-codes <sean_codes@outlook.com>
2017-10-23 00:46:32 -0500

    use array as settings argument

--- example/cw/server/server.php
+++ example/cw/server/server.php
@@ -4,16 +4,31 @@
 //-----------------------------------------------------------------------------
    require '../../../lib/server/Connect.class.php';
 
-   // Read Data
-   $uid = $_GET["uid"];
-   $room = $_GET["room"];
-   $data = json_decode($_GET["data"]);
-   $path = './Activity';
-
    // Do the darn thing!
-   $connect = new Connect($uid, $room, $data, $path, array(
-      'message'=> function($connect, $data){
+   $connect = new Connect([
+      'room' => $_GET['room'],
+      'socket' => $_GET['socket'],
+      'data' => json_decode($_GET['data']),
+      'path' => './Activity',
+      'speed' => 1/30,
+      'methods' => [
+         'message' => function($connect, $socket, $data) {
+            session_id($socket);
+            session_start();
+            $data->uid = $_SESSION['uid'];
+            error_log($_SESSION['uid']);
+            session_write_close();
+            $connect->multicast($data);
+         }
+      ],
+      'connect' => function($connect, $socket) {
+         session_id($socket);
+         session_start();
+         $_SESSION['uid'] = time();
+         session_write_close();
+      },
+      'disconnect' => function($connect, $socket, $data) {
          $connect->broadcast($data);
       }
-   ));
+   ]);
 ?>
