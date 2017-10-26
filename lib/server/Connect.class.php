bbd6c87f97e690ca97c6c20b70937b923ba50713
sean-codes <sean_codes@outlook.com>
2017-10-26 00:54:45 -0500

    wefit to use rooms object

--- lib/server/Connect.class.php
+++ lib/server/Connect.class.php
@@ -2,48 +2,52 @@
 //-----------------------------------------------------------------------------
 // Incoming Message Functions
 //-----------------------------------------------------------------------------
+require 'Rooms.class.php';
+
 class Connect {
 
+	public $room;
+	public $rooms;
 	private $socket;
 	private $path_rooms;
 	private $path_room;
 	private $timeout = 5;
+	private $socketTimeout = 10;
 	private $connect;
 	private $disconnect;
 	private $methods;
-	private $data;
-	private $room;
+	private $packet;
 	private $speed;
 
 	public function __construct($settings) {
 		$this->path = $settings['path'];
-		$this->path_rooms = "{$this->path}/";
-		$this->path_room = $this->path_rooms . $settings['room'] . '/';
-		$this->socket = $settings['socket'];
-		$this->connect = $settings['connect'];
+		$this->rooms = new Rooms($this->path);
+		$this->rooms->create( $this->room );
+
 		$this->disconnect = $settings['disconnect'];
+		$this->connect = $settings['connect'];
 		$this->methods = $settings['methods'];
+		$this->socket = $settings['socket'];
+		$this->packet = $settings['packet'];
 		$this->room = $settings['room'];
-		$this->data = $settings['data'];
 		$this->speed = $settings['speed'];
 
-		$this->room_create($this->path_room);
-		$this->socket_init();
+		$this->socket_open();
 		$this->parse();
 		return;
 	}
 
 	private function parse() {
-		if(empty($this->data)) {
+		if(empty($this->packet)) {
 			$this->data_listen();
 		} else {
-			$method = $this->data->method;
-			$this->methods[$method]($this, $this->socket, $this->data);
+			$method = $this->packet->method;
+			$this->methods[$method]($this, $this->socket, $this->packet->data);
 		}
 	}
 
 	public function data_listen() {
-		$path_socket = $this->path_room . $this->socket;
+		$path_socket = $this->rooms->socket_path($this->socket);
 		$maxTimeout = $this->timeout + time();
 		while($this->data_check( $path_socket ) && $maxTimeout > time()){
 			usleep(1000000*$this->speed);
@@ -67,25 +71,38 @@ class Connect {
 		file_put_contents($path_socket, time());
 	}
 
-	public function broadcast( $data ) {
-		$sockets = $this->get_sockets($this->room);
-		foreach($sockets as $socket){
-			$this->socket_write($socket, $data);
+	public function broadcast( $socket, $data ) {
+		$socket_room = $this->rooms->socket_find( $socket );
+
+		error_log( 'Broadcast: ' . $socket . ' room: ' . $socket_room );
+		$sockets = $this->rooms->sockets_in_room( $socket_room );
+		foreach( $sockets as $socket ){
+			$this->socket_write( $socket, $data );
 		}
 	}
 
-	public function multicast( $data ) {
-		$sockets = $this->get_sockets($this->room);
-		foreach($sockets as $socket){
-			if($this->socket != $socket){
-				$this->socket_write($socket, $data);
+	public function multicast( $socket, $data ) {
+		$socket_room = $this->rooms->socket_find( $socket );
+		error_log( 'multicast: ' . $socket . ' room: ' . $socket_room );
+		$sockets = $this->rooms->sockets_in_room( $socket_room );
+
+		foreach( $sockets as $send_socket ){
+			if( $socket != $send_socket ){
+				$this->socket_write( $send_socket, $data );
 			}
 		}
 	}
 
-	private function socket_write($socket, $data) {
+	public function unicast( $socket, $data ) {
+		error_log('unicasting: ' . json_encode($data));
+		$this->socket_write( $socket, $data );
+	}
+
+	private function socket_write( $socket, $data ) {
+		$socket_path = $this->rooms->socket_path( $socket );
+		if( empty( $socket_path ) || !file_exists( $socket_path ) ) return;
 		// Open file and read last poll
-		$file_socket = fopen($this->path_room . $socket, 'r+');
+		$file_socket = fopen($socket_path, 'r+');
 		flock($file_socket, LOCK_EX);
 		$socket_last_poll = time() - fread($file_socket, 10);
 
@@ -95,57 +112,40 @@ class Connect {
 		fclose($file_socket);
 
 		// Close if old socket
-		if($socket_last_poll > 20){ $this->socket_close($this->room, $socket); }
+		if($socket_last_poll > $this->socketTimeout){ $this->socket_close( $socket ); }
 	}
 
-	private function socket_init() {
-		$this->room_set();
-		$newSocket = !file_exists($this->path_room . $this->socket);
-		$file = fopen($this->path_room . $this->socket, 'c');
+	private function socket_open() {
+		$socket_path = $this->rooms->socket_path( $this->socket );
+		$connect = false;
+
+		if( empty( $socket_path ) ) {
+			$socket_path = $this->rooms->socket_new( $this->room, $this->socket);
+			$connect = true;
+		}
+
+		$file = fopen($socket_path, 'c');
 		fwrite($file, time());
 		fclose($file);
 
-		if($newSocket){
-			error_log('new socket');
-			call_user_func_array($this->connect, [ $this, $this->socket, $this->data ]);
+		if($connect){
+			call_user_func_array($this->connect, [ $this, $this->socket ]);
 		}
 	}
 
-	private function room_create($path_room) {
-		if (!file_exists($path_room)) {
-    		mkdir($path_room, 0777, true);
-		}
-	}
-
-	private function room_set() {
-		foreach($rooms = $this->get_rooms() as $room){
-			if($room != $this->room){
-				$sockets_in_room = $this->get_sockets($room);
-				$socket_search = array_search($this->socket, $sockets_in_room);
-				if($socket_search){
-					$this->socket_move($room, $this->room, $this->socket);
-					return;
-				}
-			}
-		}
-	}
-
-	private function socket_move($room_old, $room_new, $socket) {
-		$path_old = $this->path_rooms . "$room_old/$socket";
-		$path_new = $this->path_rooms . "$room_new/$socket";
+	private function socket_change_room( $socket, $room_new ) {
+		$path_old = $this->rooms->socket_path( $socket );
+		$this->rooms->socket_move( $socket, $room );
+		$path_new = $this->rooms->socket_path( $socket );
 		rename($path_old, $path_new);
 	}
 
-	private function socket_close($room, $socket) {
-		unlink($this->path_rooms . $room . '/' . $socket);
-	}
-
-	private function get_rooms() {
-		return array_diff(scandir($this->path_rooms), array('..', '.'));
-	}
-
-	private function get_sockets($room) {
-		return array_diff(scandir($this->path_rooms . $room), array('..', '.'));
+	private function socket_close( $socket ) {
+		unlink( $this->rooms->socket_path( $socket ));
+		call_user_func_array($this->disconnect, [ $this, $socket ]);
+		if( $this->rooms->socket_close( $socket ) ) {
+			error_log('Disconnect Successful: ' . $socket);
+		}
 	}
 }
 ?>
